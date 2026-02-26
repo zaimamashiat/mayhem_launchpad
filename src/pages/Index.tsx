06903749abe92cd574b66b9a6867e07b755b2ce5
utshobbose <boseadidev1@gmail.com>
2026-02-27 01:37:44 +0600

    Removing TechStack

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -35,7 +35,7 @@ const Index = () => {
       {/* Sections */}
       <Hero />
       <Services />
-      <TechStack />
+      {/* <TechStack /> */}
       <Projects />
       <WhyChooseUs />
       <MeetTheTeam />
