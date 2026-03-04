9ea49bd9468744e64764d6822659468c6f4b1f65
Zaima Mashiat <153401737+zaimamashiat@users.noreply.github.com>
2026-03-05 02:58:08 +0600

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,31 @@
+// main.tsx
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Index"; // Your homepage
+import About from "./pages/Careers";
+import Services from "./pages/Index"; 
+import Contact from "./pages/Contact";
+import Blog from "./pages/Blog";
+import Product from "./pages/Products";
+import Testimonials from "./pages/Testimonials";
+
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Root rendering
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<Product />} />
+        <Route path="/products" element={<Testimonials />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
